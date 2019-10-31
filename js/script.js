b5de1b7f3a3d9b0e9ddc7f4ef75316af626ad9b2
xiluna <xilaluna2@gmail.com>
2019-10-31 01:12:49 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+let compRPS = ['rock', 'paper', 'scissors'];
+
+$('#shoot').click(function() {
+    let userInput = $('#input').val();
+    $('#userChoice').append(userInput);
+    let compChoice = compRPS[Math.floor(Math.random() * compRPS.length)];
+    console.log(compChoice);
+});
 
 //GLOBAL VARIABLES
 /* global $ */
