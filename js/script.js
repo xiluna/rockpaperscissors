0dc87d2318fa913e822822b2dc913814ec462d46
xiluna <xilaluna2@gmail.com>
2019-11-13 05:31:52 +0000

    It Is DONE

--- js/script.js
+++ js/script.js
@@ -5,9 +5,29 @@ let compRPS = ['rock', 'paper', 'scissors'];
 
 $('#shoot').click(function() {
     let userInput = $('#input').val();
-    $('#userChoice').append(userInput);
+    let user = userInput.toLowerCase();
+    $('#userChoice').text(user);
     let compChoice = compRPS[Math.floor(Math.random() * compRPS.length)];
-    console.log(compChoice);
+    $('#computerChoice').text(compChoice);
+    
+    if (compChoice === user) {
+      $('#result').text('Its a Draw!!! reShoot!');
+
+    }else if (user === 'paper' && compChoice === 'rock') {
+      $('#result').text('Paper beats rock!!! User WINS!');
+    }else if (compChoice === 'paper' && user === 'rock') {
+      $('#result').text('Paper beats rock!!! Computer WINS!');
+
+    }else if (user === 'scissors' && compChoice === 'paper') {
+      $('#result').text('Scissors beats paper!!! User WINS!');
+    }else if (compChoice === 'scissors' && user === 'paper') {
+      $('#result').text('Scissors beats paper!!! Computer WINS!');
+
+    }else if (user === 'rock' && compChoice === 'scissors') {
+      $('#result').text('Rock beats scissors!!! User WINS!');
+    }else if (compChoice === 'rock' && user === 'scissors') {
+      $('#result').text('Rock beats scissors!!! Computer WINS!');
+    }
 });
 
 //GLOBAL VARIABLES
